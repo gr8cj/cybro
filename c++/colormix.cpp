0b7297387c5e10c335c6269d571ab837c321b1c3
chandan jha <gr8cjcode@gmail.com>
2024-11-25 17:37:42 +0530

    some question in c++

---
+++ c++/colormix.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    int red=1;
+    int orange=2;
+    int =3;
+    int red=4;
+    int red=5;
+    int red=6;
+    
+    if(1 &&2){
+        cout<<"dark red"
+    }
+    else if(3 &&4){
+        cout<<"dark red"
+    }
+    else if(5 &&6){
+        
+    }
+    return 0;
+}
