405eabdc858852a33756800bb41679fad9232258
chandan jha <gr8cjcode@gmail.com>
2024-12-23 18:38:33 +0530

    this iss identifier

--- python/listq&a.py
+++ python/listq&a.py
@@ -1,3 +1,3 @@
 # Given the list fruits = ['apple', 'banana', 'cherry', 'date'], how do you access the first and last elements?
 fruits = ['apple', 'banana', 'cherry', 'date']
-print(fruits[0,-1])
+print(fruits[0:4:3])
