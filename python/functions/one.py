c89435f2a4a430baf8fdc062c86302a9260b0b0b
chandan jha <gr8cjcode@gmail.com>
2025-01-21 01:01:33 +0530

    this is a function that can we use to itrate

---
+++ python/functions/one.py
@@ -0,0 +1,9 @@
+def leapyear(year):
+    return "no" if year%4==0 else "not a leap year"
+leaypear(2000)
+
+def twomax(a,b):
+    return a if a>b else b
+def three(a,b,c):
+    return maxtew(a,maxtwo(b,c))
+    
