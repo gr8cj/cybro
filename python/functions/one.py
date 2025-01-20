5c46be64846ae19d4cc500c868dbb4eb016c9caa
chandan jha <gr8cjcode@gmail.com>
2025-01-21 01:04:29 +0530

    this is recurtion

--- python/functions/one.py
+++ python/functions/one.py
@@ -4,6 +4,19 @@ leaypear(2000)
 
 def twomax(a,b):
     return a if a>b else b
+twomax(102,32)
 def three(a,b,c):
     return maxtew(a,maxtwo(b,c))
-    
+three(1,2,3)
+
+
+
+
+
+
+def sum(a):
+    return a + (sum-1)
+sum(10)
+def mul(n):
+    return n + (mul-1)
+mul(10)
