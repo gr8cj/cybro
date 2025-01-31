74dd1d4dc2bd01b3044aa1efab079a1f6d1d6c73
chandan jha <gr8cjcode@gmail.com>
2025-01-31 11:44:27 +0530

    thisdshdskl1

--- SQL/constraints.sql
+++ SQL/constraints.sql
@@ -2,3 +2,20 @@
 #alter table tablename add constraints check(colname);
 alter table customers add constraint check(age>=18);
 desc customers;
+
+
+create database collage;
+use collage;
+create table stu(sid int primary key,sname varchar(30) not null,rollno int unique,age tinyint,check (age >=18) );
+select * from stu;
+desc stu;
+alter table stu add column subject varchar(10);
+
+create table subjects(sid int  primary key,  sname varchar(20) not null,allowed varchar(20) default "yes");
+
+create table class(cid int primary key,section varchar(12), subid int ,foreign key(subid) references subjects(sid));
+
+rename table stu to student;
+rename table student to stu;
+show databases;
+desc stu;
