5ccc37f5deb7ab6b90695f755833f6b520323f19
chandan jha <gr8cjcode@gmail.com>
2025-02-18 23:22:37 +0530

    thi is sql

--- SQL/join.sql
+++ SQL/join.sql
@@ -4,3 +4,12 @@ select productname,p.productcode from products p
 left join orderdetails od 
 on p.productcode=od.productcode
 where od.productcode is null;
+
+
+-- Question-2 Find the colors of boats reserved by  Lubber?
+
+
+select color from boats b
+inner join Reserves r on b.bid=r.bid
+inner join sailors s on r.sid=s.sid
+where sname="lubber";
